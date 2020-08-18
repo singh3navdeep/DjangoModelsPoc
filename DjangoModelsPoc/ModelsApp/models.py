52e51477bb83e73f1d3e33a6c04471f2e4c228f5
navdeepsingh <navdeep3singh@yahoo.com>
2020-08-18 21:58:55 +0530

    changed model to user

--- DjangoModelsPoc/ModelsApp/models.py
+++ DjangoModelsPoc/ModelsApp/models.py
@@ -5,31 +5,11 @@ from django.urls import reverse
 
 # Create your models here.
 
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True, max_length=255)
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    author = models.TextField()
-
-    def get_absolute_url(self):
-        return reverse('model_post_detail', args=[self.slug])
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+    email = models.EmailField(max_length=75)
+    password = models.CharField(max_length=42)
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super(Post, self).save(*args, **kwargs)
-
-    class Meta:
-        ordering = ['created_on']
-
-        def __unicode__(self):
-            return self.title
-
-class Comment(models.Model):
-    name = models.CharField(max_length=42)
-    email = models.EmailField(max_length=75)
-    website = models.URLField(max_length=200, null=True, blank=True)
-    content = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created_on = models.DateTimeField(auto_now_add=True)
+        super(User, self).save(*args, **kwargs)
